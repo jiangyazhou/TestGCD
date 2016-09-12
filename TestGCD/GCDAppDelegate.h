29b80d13de656ec818bf5f9ee5d6f662f9417993
Asian Jiang <jyz@maomao-2.local>
2016-09-12 09:28:26 +0800

    add files

---
+++ TestGCD/GCDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  GCDAppDelegate.h
+//  TestGCD
+//
+//  Created by jiang yazhou on 13-8-29.
+//  Copyright (c) 2013年 Jiang Yazhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class GCDViewController;
+
+@interface GCDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) GCDViewController *viewController;
+
+@end
---
+++ TestGCD/GCDViewController.h
@@ -0,0 +1,13 @@
+//
+//  GCDViewController.h
+//  TestGCD
+//
+//  Created by jiang yazhou on 13-8-29.
+//  Copyright (c) 2013年 Jiang Yazhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCDViewController : UIViewController
+
+@end
